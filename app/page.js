dff1e2381d5cfb2e7e16472efa2bdbf391807cdc
unknown <ruben.a.her@gmail.com>
2023-12-12 10:46:24 -0300

    9c

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
             filter: "blur(0px)",
             transition: { duration: 0.4, delay: 0.5 },
           }}
-          className="w-full h-full flex object-cover   rounded-full "
+          className="w-full h-full flex object-cover rounded-full "
           src="/img/1.png"
           alt=""
         />
@@ -38,14 +38,14 @@ export default function Home() {
                 filter: "blur(0px)",
                 transition: { duration: 0.4, delay: 0.5 },
               }}
-              className="w-72 h-82 h-72 flex object-cover mx-auto"
+              className="w-72 h-82 h-72 flex mt-20 object-cover mx-auto"
               src="/img/chela50.png"
               alt=""
             />
 
-            <div className="  flex-col md:flex-row   my-16 flex items-center md:justify-between">
+            <div className="  flex-col md:flex-row   my-4 flex items-center md:justify-between">
               <div className=" sm:order-2   max-md:mt-10 flex md:block md:flex-row items-center  flex-col md:order-1 order-2">
-                <h1 className="text-6xl font-custom text-center">
+                <h1 className="text-6xl font-custom text-center text-[#FFD700]">
                   Festejemos Juntos
                 </h1>
 
@@ -57,10 +57,10 @@ export default function Home() {
                     filter: "blur(0px)",
                     transition: { duration: 0.2, delay: 0.3 },
                   }}
-                  className="  text-lg max-md:text-center  my-3 text-neutral-500 dark:text-neutral-300"
+                  className="  text-lg text-center  my-3 text-[#FFD700]"
                 >
-                  La mayor alegría para mí sería tenerte a mi lado compartiendo
-                  este día tan especial.
+                  La mayor alegría, para mí, sería tenerte a mi lado
+                  compartiendo este día tan especial.
                 </motion.p>
 
                 <motion.img
@@ -77,16 +77,16 @@ export default function Home() {
                 />
 
                 <div>
-                  <h2 className=" text-4xl text-center dark:text-white text-[#353849] font-custom ">
-                    16 de Diciembre
+                  <h2 className=" text-4xl text-center text-[#FFD700] font-custom ">
+                    Sábado 16 de diciembre
                   </h2>
-                  <h4 className=" text-2xl text-center dark:text-white text-[#353849] font-custom ">
+                  <h4 className=" text-2xl text-center text-[#FFD700] font-custom ">
                     21:30 hs
                   </h4>
-                  <h2 className=" text-4xl text-center dark:text-white text-[#353849] font-custom ">
+                  <h2 className=" text-4xl text-center text-[#FFD700] font-custom ">
                     Hotel Asturias - Cafayate
                   </h2>
-                  <p className="text-center dark:text-neutral-300 text-[#666D80] text-lg">
+                  <p className="text-center text-[#FFD700] text-lg">
                     Ingreso por calle Miguel Hurtado
                   </p>
                 </div>
@@ -97,7 +97,7 @@ export default function Home() {
                       "https://api.whatsapp.com/send/?phone=%2B5403868411475&text=Hola,+confirmo+mi+asistencia+al+cumpleaños+de+Chela&type=phone_number&app_absent=0"
                     }
                   >
-                    <div className="bg-[#ecdf2a] h-6 w-100 w-fit px-3 py-6 rounded-full flex items-center justify-center gap-x-2">
+                    <div className="bg-[#FFD700] h-6 w-100 w-fit px-3 py-6 rounded-full flex items-center justify-center gap-x-2">
                       <div className="w-2 h-2 rounded-full animate-ping dark:animate-none bg-[#28C780]" />
                       <p className="text-[#000000] lg:inline font-bold">
                         Confirmar asistencia
@@ -130,10 +130,10 @@ export default function Home() {
 
                 <div className="mx-auto max-w-md">
                   <div>
-                    <h2 className=" text-6xl text-center font-custom dark:text-white text-[#353849] ">
+                    <h2 className=" text-6xl text-center font-custom text-[#FFD700] ">
                       Tu compañía es el regalo más preciado.
                     </h2>
-                    <p className="text-center mt-3 dark:text-neutral-300 text-[#666D80] text-lg">
+                    <p className="text-center mt-3 text-[#FFD700] text-lg">
                       Sin embargo, si sientes el deseo de obsequiarme algo y no
                       sabes qué, he creado un enlace de pago donde puedes
                       contribuir con lo que desees. El enlace acepta pagos con
@@ -167,8 +167,9 @@ export default function Home() {
                 />
 
                 <div>
-                  <h2 className=" text-6xl text-center dark:text-white text-[#353849]  font-custom">
-                    Agradezco de antemano tu amor y amistad. Con aprecio,
+                  <h2 className=" text-6xl text-center text-[#FFD700] font-custom">
+                    Agradezco de antemano tu amor y amistad.<br></br> Con
+                    aprecio,
                     <motion.img
                       initial={{ x: 100, opacity: 0, filter: "blur(50px)" }}
                       animate={{
